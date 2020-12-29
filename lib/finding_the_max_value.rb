2a72c0b35bb3b9f5b297de91ef554fa4a545950c
Jamie Lawrence-Hurt <jpham1109@gmail.com>
2020-12-29 22:40:30 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,7 +1,10 @@
 def find_max_value(array)
   count = 0 
+  array_sorted = array.sort 
   while count < array.length do
-    
+    if array[count] == array_sorted[array_sorted.length - 1]
+      return count 
+    end
     counter += 1 
   end 
 end
